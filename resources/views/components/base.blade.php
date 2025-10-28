1a50cf6a2a375dff9176b3e5a5a2b88d3d120973
LIrbe <linards2000@gmail.com>
2025-10-28 07:02:30 +0200

    Navigācijas pamati

--- resources/views/components/base.blade.php
+++ resources/views/components/base.blade.php
@@ -3,10 +3,20 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css" >
 
         <title>Apses Spēks</title>
     </head>
     <body class="antialiased">
+        <img id="banner">
+        <div id="navigation">
+            <a>Sākumlapa</a>
+            <a>Galerija</a>
+            <a>Objekti</a>
+            <a>Karte</a>
+            <a>Rezervācijas</a>
+            <a>Kontakti</a>
+        </div>
         {{ $slot }}
     </body>
 </html>
